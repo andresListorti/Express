c4ea4358f1deac30dee9fe1b74df332f40c0821b
andresListorti <andreslistorti@gmail.com>
2022-08-17 11:53:34 -0300

    exp

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-const http = require("http");
+const express = require("express"); //esto es lo mismo q hacerlo en const express y luego express() en la const app
+const path = require("path");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  console.log("user hit server");
-  res.end(`<a href='/'>Click Me</a>`);
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
 });
 
-server.listen(5000);
+app.all("*", (req, res) => {
+  res.status(404).send("Resource not found");
+});
+
+app.listen(5000, () => {
+  console.log("server is listening on port 5000...");
+});
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
